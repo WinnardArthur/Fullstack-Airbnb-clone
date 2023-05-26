d4ca12a9fb29516a9b172a1178525126c2e5efb5
Winnard Arthur <winnardarthur7@gmail.com>
2023-05-26 21:22:33 +0000

    create header user interface

---
+++ app/components/navbar/MenuItem.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+interface MenuItemProps {
+    onClick: () => void;
+    label: string;  
+}
+
+import React from 'react'
+
+const MenuItem: React.FC<MenuItemProps> = ({onClick, label}) => {
+  return (
+      <div onClick={onClick} className='px-4 py-3 hover:bg-neutral-500 transition font-semibold'>
+          {label}
+    </div>
+  )
+}
+
+export default MenuItem
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,17 @@
-import './globals.css'
+import { Nunito } from 'next/font/google';
+import './globals.css';
+import Navbar from './components/navbar/Navbar';
 
+const font = Nunito({
+  subsets: ['latin']
+})
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
+
+
   return (
     <html lang="en">
       {/*
@@ -12,7 +19,10 @@ export default function RootLayout({
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
       <head />
-      <body>{children}</body>
+      <body className={font.className}>
+        <Navbar />
+        {children}
+      </body>
     </html>
   )
 }
