2ff6f527bd8145eaa71aae4ae950d5428b3e0e91
Winnard Arthur <winnardarthur7@gmail.com>
2023-06-02 08:52:56 +0000

    feat: implement favorite feature

---
+++ app/hooks/useFavorite.ts
@@ -0,0 +1,65 @@
+import axios from 'axios';
+import { useRouter } from 'next/navigation';
+import { useCallback, useMemo } from 'react';
+import { SafeUser } from '../types';
+import useLoginModal from './useLoginModal';
+import { toast } from 'react-hot-toast';
+
+interface IUseFavorite {
+    listingId: string;
+    currentUser?: SafeUser | null;
+}
+
+const useFavorite = ({ listingId, currentUser}: IUseFavorite) => {
+    const router = useRouter();
+    const loginModal = useLoginModal();
+    
+    // Check is listing is a favorite or not
+    const hasFavorited = useMemo(() => {
+        const list = currentUser?.favoriteIds || [];
+
+        return list.includes(listingId);
+    }, [currentUser, listingId]) 
+    
+    // Toggle between favorite and not favorite
+    const toggleFavorite = useCallback(async (
+    e: React.MouseEvent<HTMLDivElement>
+    ) => {
+        e.stopPropagation();
+
+        if (!currentUser) {
+            return loginModal.onOpen();
+        }
+
+        try {
+            let request;
+
+            if (hasFavorited) {
+                request = () => axios.delete(`/api/favorites/${listingId}`)
+            } else {
+                request = () => axios.post(`/api/favorites/${listingId}`)
+            }
+
+            await request();
+            router.refresh();
+            toast.success('Success')
+        } catch(error) {
+            toast.error('Something went wrong')
+        }
+
+    }, [
+        currentUser,
+        hasFavorited,
+        listingId,
+        loginModal,
+        router
+    ])
+
+    return {
+        hasFavorited,
+        toggleFavorite
+    }
+}
+
+export default useFavorite;
+
