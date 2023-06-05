d5bf4111b4c8df95ffeb03b60b0bebac5d460659
Winnard Arthur <winnardarthur7@gmail.com>
2023-06-05 09:35:20 +0000

    feat: display filters on search bar

--- app/components/navbar/Search.tsx
+++ app/components/navbar/Search.tsx
@@ -1,22 +1,70 @@
 "use client";
 
+import { useMemo } from "react";
 import { BiSearch } from "react-icons/bi";
-import useSearchModal from '@/app/hooks/useSearchModal';
+import { useSearchParams } from "next/navigation";
+import useSearchModal from "@/app/hooks/useSearchModal";
+import useCountries from "@/app/hooks/useCountries";
+import { differenceInDays } from "date-fns";
 
 const Search = () => {
   const searchModal = useSearchModal();
+  const params = useSearchParams();
+  const { getByValue } = useCountries();
+
+  const locationValue = params?.get("locationValue");
+  const startDate = params?.get("startDate");
+  const endDate = params?.get("endDate");
+  const guestCount = params?.get("guestCount");
+
+  // Location label
+  const locationLabel = useMemo(() => {
+    if (locationValue) {
+      return getByValue(locationValue as string)?.label;
+    }
+
+    return "Anywhere";
+  }, [getByValue, locationValue]);
+
+  // Duration label
+  const durationLabel = useMemo(() => {
+    if (startDate && endDate) {
+      const start = new Date(startDate as string);
+      const end = new Date(endDate as string);
+
+      let diff = differenceInDays(end, start);
+
+      if (diff === 0) {
+        diff = 1;
+      }
+
+      return `${diff} ${diff === 1 ? 'Day' : 'Days'}`;
+    }
+
+    return "Any Week";
+  }, [startDate, endDate]);
+
+  // Guest Label
+  const guestLabel = useMemo(() => {
+    if (guestCount) {
+      return `${guestCount} ${Number(guestCount) === 1 ? "Guest" : "Guests"}`;
+    }
+
+    return "Add Guests";
+  }, [guestCount]);
 
   return (
     <div
       onClick={searchModal.onOpen}
-      className="border-[1px] rounded-full w-full md:w-auto py-2 shadow-sm hover:shadow-md transition cursor-pointer">
+      className="border-[1px] rounded-full w-full md:w-auto py-2 shadow-sm hover:shadow-md transition cursor-pointer"
+    >
       <div className="flex flex-row items-center justify-center">
-        <div className="text-sm text-semibold px-6">Anywhere</div>
+        <div className="text-sm text-semibold px-6">{locationLabel}</div>
         <div className="hidden sm:block text-sm font-semibold px-6 border-x-[1px] flex-1 text-center">
-          Any Week
+          {durationLabel}
         </div>
         <div className="text-sm pl-6 pr-2 text-gray-600 flex flex-row gap-3 items-center">
-          <div className="hidden sm:block">Add Guests</div>
+          <div className="hidden sm:block">{guestLabel}</div>
           <div className="p-2 bg-rose-500 rounded-full text-white">
             <BiSearch size={18} />
           </div>
